eff7c1d623e209d24dc596d9e63c9b630288f882
Beo <102859541+Pagoda2000@users.noreply.github.com>
2022-12-19 15:30:30 +0000

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,4 +1,5 @@
 import streamlit
+streamlit.title('Healthy Breakfast Menu')
 streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Omega 3 & Blueberry Oatmeal')
 streamlit.title('Kale, spinach & Rocket Smoothie')
